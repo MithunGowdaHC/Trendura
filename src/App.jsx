b3937d03ca665a978a4581f92df42d06124a8f48
MithunGowda <mithungowdahc12a@gmail.com>
2024-08-25 00:02:33 +0530

    day1-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ABout from "./pages/ABout";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
+      <Navbar/>      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element = {<ABout/>}/>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/collection" element={<Collection/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/place-order" element={<PlaceOrder/>}/>
+        <Route path="/product:ProductId" element={<Product/>}/>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
